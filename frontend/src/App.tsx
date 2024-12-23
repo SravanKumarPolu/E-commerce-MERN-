52e83180eedd2aede1fec0e3f807f0574f6f542f
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2024-12-23 22:52:04 +0530

    pages created.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Route, Routes } from 'react-router-dom'
+
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import NavBar from './components/NavBar'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Product from './pages/Product'
+import SearchBar from './components/SearchBar'
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="px-4 sm:px-6 md:px-8 lg:px-10 xl:px-12 max-w-screen-xl mx-auto">
+        <NavBar />
+        <h1>E-commerce(MERN)</h1>
+        <SearchBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/collection' element={<Collection />} />
+          <Route path='/Contact' element={<Contact />} />
+          <Route path='/Product/:productId' element={<Product />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/place-order' element={<PlaceOrder />} />
+
+          <Route path='/orders' element={<Orders />} />
+        </Routes>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
