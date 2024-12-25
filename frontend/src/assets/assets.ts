7913937203a0fad1d3eea414dd2b71f24b43f963
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2024-12-25 22:21:57 +0530

    Update assets.ts

--- frontend/src/assets/assets.ts
+++ frontend/src/assets/assets.ts
@@ -9,24 +9,42 @@ import hero_img from './hero_img.png'
 import logo from './logo.png'
 import menu_icon from './menu_icon.png'
 import p_img1 from './p_img1.png'
-import p_img10_1 from './p_img10_1.png'
-import p_img11_1 from './p_img11_1.png'
-import p_img12_1 from './p_img12_1.png'
-import p_img13_1 from './p_img13_1.png'
+import p_img10_1 from './p_img10_1.png';
+import p_img10_2 from './p_img10_2.png';
+import p_img11_1 from './p_img11_1.png';
+import p_img11_2 from './p_img11_2.png';
+import p_img12_1 from './p_img12_1.png';
+import p_img12_2 from './p_img12_2.png';
+import p_img13_1 from './p_img13_1.png';
+import p_img13_2 from './p_img13_2.png';
+import p_img13_3 from './p_img13_3.png';
 import p_img14_1 from './p_img14_1.png';
+import p_img14_2 from './p_img14_2.png';
+import p_img14_3 from './p_img14_3.png';
 import p_img15_1 from './p_img15_1.png';
 import p_img16_1 from './p_img16_1.png';
+import p_img16_2 from './p_img16_2.png';
 import p_img17_1 from './p_img17_1.png';
+import p_img17_2 from './p_img17_2.png';
 import p_img18_1 from './p_img18_1.png';
+import p_img18_2 from './p_img18_2.png';
 import p_img19_1 from './p_img19_1.png';
+import p_img19_2 from './p_img19_2.png';
 import p_img1_2 from './p_img1_2.png'
 import p_img1_3 from './p_img1_3.png'
 import p_img20_1 from './p_img20_1.png';
+import p_img20_2 from './p_img20_2.png';
+import p_img20_3 from './p_img20_3.png';
 import p_img21_1 from './p_img21_1.png';
 import p_img22_2 from './p_img22_2.png';
 import p_img23_1 from './p_img23_1.png';
+import p_img23_2 from './p_img23_2.png';
+import p_img23_3 from './p_img23_3.png';
 import p_img24_1 from './p_img24_1.png';
+import p_img24_2 from './p_img24_2.png';
 import p_img25_1 from './p_img25_1.png';
+import p_img25_2 from './p_img25_2.png';
+import p_img25_3 from './p_img25_3.png';
 import p_img26_1 from './p_img26_1.png';
 import p_img27_1 from './p_img27_1.png';
 import p_img28_1 from './p_img28_1.png';
@@ -35,15 +53,22 @@ import p_img2_1 from './p_img2_1.png'
 import p_img2_2 from './p_img2_2.png'
 import p_img2_3 from './p_img2_3.png'
 import p_img2_4 from './p_img2_4.png'
-import p_img3 from './p_img3.png'
 import p_img30_1 from './p_img30_1.png';
 import p_img31_1 from './p_img31_1.png'
 import p_img32_1 from './p_img32_1.png'
+import p_img3_1 from './p_img3_1.png'
+import p_img3_2 from './p_img3_2.png'
+import p_img3_3 from './p_img3_3.png'
 import p_img4_1 from './p_img4_1.png';
+import p_img4_2 from './p_img4_2.png';
+import p_img4_3 from './p_img4_3.png';
 import p_img5_1 from './p_img5_1.png';
+import p_img5_2 from './p_img5_2.png';
 import p_img6_1 from './p_img6_1.png';
+import p_img6_2 from './p_img6_2.png';
 import p_img7_1 from './p_img7_1.png';
 import p_img8_1 from './p_img8_1.png';
+import p_img8_2 from './p_img8_2.png';
 import p_img9_1 from './p_img9_1.png';
 import profile_icon from './profile_icon.png'
 import quality_icon from './quality_icon.png'
@@ -105,7 +130,7 @@ export const products = [
         name: "iPad Pro 11",
         description: "Apple Intelligence helps you write, express yourself, and get things done effortlessly while giving you peace of mind with groundbreaking privacy protections.",
         price: 620,
-        image: [p_img3],
+        image: [p_img3_1,p_img3_2,p_img3_3],
         category: "iPad",
         subCategory: " Pro ",
         colors: ["Silver", "Black ", "White"],
@@ -117,7 +142,7 @@ export const products = [
         name: "iPad 5 Plus",
         description: "The A14 Bionic chip lets you run multiple apps and work smoothly between them",
         price: 500,
-        image: [p_img4_1],
+        image: [p_img4_1, p_img4_2, p_img4_3],
         category: "iPad",
         subCategory: "Plus",
         colors: ["Silver", "Black ", "White", "Yellow"],
@@ -129,7 +154,7 @@ export const products = [
         name: "MacBook Pro 14",
         description: "Supercharged by M4, M4 Pro or M4 Max, MacBook Pro empowers you to take on the most demanding projects",
         price: 1530,
-        image: [p_img5_1],
+        image: [p_img5_1, p_img5_2],
         category: "Laptops",
         subCategory: "Pro",
         colors: ["Silver", "Black ",],
@@ -141,7 +166,7 @@ export const products = [
         name: "MacBook 14 Plus",
         description: "The powerful 8-core CPU and up to 10-core GPU of the Apple M2 chip keep things running smoothly",
         price: 1500,
-        image: [p_img6_1],
+        image: [p_img6_1, p_img6_2],
         category: "Laptop",
         subCategory: "Plus",
         colors: ["Silver", " Gold", "Black ", "white"],
@@ -165,7 +190,7 @@ export const products = [
         name: "iPhone 16 plus",
         description: "15.5 cm (6.1″) aerospace-grade aluminium design footnote 1 with durable, latest-generation Ceramic Shield front, Action button and USB-C",
         price: 900,
-        image: [p_img8_1],
+        image: [p_img8_1, p_img8_2],
         category: "iPhone",
         subCategory: "Plus",
         colors: [" Gold", "Black ", "White", "Teal", "Pink"],
@@ -190,7 +215,7 @@ export const products = [
         name: "Apple Watch Ultra 1",
         description: "With Family Setup, your kids can use Apple Watch,  footnote  § even if they don’t have their own iPhone",
         price: 380,
-        image: [p_img10_1],
+        image: [p_img10_1, p_img10_2],
         category: "Watch",
         subCategory: "Ultra",
         colors: [" Gold", "Black ", "Yellow", "Teal", "Pink"],
@@ -202,7 +227,7 @@ export const products = [
         name: "iPhone 15 ",
         description: "15.5 cm durable colour-infused glass and aluminium design footnote ◊ with Ceramic Shield front",
         price: 890,
-        image: [p_img11_1],
+        image: [p_img11_1, p_img11_2],
         category: "iPhone",
         subCategory: "Plus",
         colors: ["White", "Teal", "Ultramarine"],
@@ -214,7 +239,7 @@ export const products = [
         name: "Apple Watch Ultra 2",
         description: "The ultimate sports and adventure watch has a rugged 49mm titanium case with 100-metre water resistance  footnote  § and IP6X dust resistance.  footnote  ° Now in black",
         price: 360,
-        image: [p_img12_1],
+        image: [p_img12_1, p_img12_2],
         category: "Watch",
         subCategory: "Ultra",
         colors: ["White", "Teal"],
@@ -226,7 +251,7 @@ export const products = [
         name: "iPhone 15 Plus",
         description: ".A16 Bionic powers computational photography and smooth gaming performance with great efficiency for all-day battery life.",
         price: 920,
-        image: [p_img13_1],
+        image:  [p_img13_1, p_img13_2, p_img13_3],
         category: "iPhone",
         subCategory: "Plus",
         colors: ["White", "Teal", "Pink", "Ultramarine"],
@@ -238,7 +263,7 @@ export const products = [
         name: "MacBook Pro 13",
         description: "MacBook Pro 2020 (Touch Bar, Four Thunderbolt 3 ports) A2251 (Intel Core i7 13 Inch)",
         price: 160,
-        image: [p_img14_1],
+        image: [p_img14_1, p_img14_2, p_img14_3],
         category: "Laptop",
         subCategory: "Pro",
         colors: ["White", "Teal", "Black", "Ultramarine"],
@@ -262,7 +287,7 @@ export const products = [
         name: "Apple Watch Series 9",
         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
         price: 400,
-        image: [p_img16_1],
+        image: [p_img16_1, p_img16_2],
         category: "Watch",
         subCategory: "Plus",
         colors: ["White", "Teal", "Pink",],
@@ -274,7 +299,7 @@ export const products = [
         name: "Apple Watch Series 7",
         description: "Apple Watch Series 7 Case Sport Band.",
         price: 150,
-        image: [p_img17_1],
+        image: [p_img17_1, p_img17_2],
         category: "Watch",
         subCategory: "Pulse",
         colors: ["Black", "White", "Teal", "Pink", "Ultramarine"],
@@ -286,7 +311,7 @@ export const products = [
         name: "Apple AirPods Pro",
         description: "Redesigned for exceptional fit and audio performance. The powerful H2 chip delivers clearer calls with Voice Isolation and a new, hands-free way to interact with Siri.",
         price: 280,
-        image: [p_img18_1],
+        image: [p_img18_1, p_img18_2],
         category: "AirPods",
         subCategory: "Pro",
         colors: ["White", "Teal", "Pink",],
@@ -298,7 +323,7 @@ export const products = [
         name: "Apple AirPods Plus",
         description: "USB-C charging and up to 30 hours of listening time using the case. Footnote ¹⁴ And Find My to help you locate your AirPods 4",
         price: 260,
-        image: [p_img19_1],
+        image: [p_img19_1, p_img19_2],
         category: "Kids",
         subCategory: "Plus",
         colors: ["White", "Teal",],
@@ -310,7 +335,7 @@ export const products = [
         name: "Apple Watch Ultra 1.3",
         description: "Apple Watch Ultra 1.3 GPS 41mm Midnight Aluminium Case with Midnight Sport Band.",
         price: 370,
-        image: [p_img20_1],
+        image: [p_img20_1, p_img20_2, p_img20_3],
         category: "Watch",
         subCategory: "Ultra",
         colors: ["Black", "White", "Teal", "Pink",],
@@ -346,7 +371,7 @@ export const products = [
         name: "Apple iMac 27",
         description: "Imac performance is at an all-time high with faster Intel Core processors, powerful Radeon Pro 500-Series graphics, and stunning Retina 4K and 5K displays.",
         price: 180,
-        image: [p_img23_1],
+        image: [p_img23_1, p_img23_2, p_img23_3],
         category: "TV",
         subCategory: "Ultra",
         colors: ["Black", "White", "Teal"],
@@ -358,7 +383,7 @@ export const products = [
         name: "iPhone 14",
         description: "A Pro-level Main camera and advanced image processing for sensational shots in all kinds of light.",
         price: 710,
-        image: [p_img24_1],
+        image: [p_img24_1, p_img24_2],
         category: "iPhone",
         subCategory: "Plus",
         colors: ["White", "Teal", "Black", "Ultramarine"],
@@ -370,7 +395,7 @@ export const products = [
         name: "iPhone 14 Pro",
         description: "Superfast 5G. With SmartData mode, iPhone downshifts automatically to save power when you don’t need all that speed.",
         price: 679,
-        image: [p_img25_1],
+        image: [p_img25_1, p_img25_2, p_img25_3],
         category: "iPhone",
         subCategory: "Pro",
         colors: ["White", "Teal", "Black", "Ultramarine", "Pink"],
