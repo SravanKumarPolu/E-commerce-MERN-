c6449d3088eebeaf212eef1bdd371e005ee555e9
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2024-12-25 23:25:53 +0530

    added svg for navbar

--- frontend/src/assets/assets.ts
+++ frontend/src/assets/assets.ts
@@ -1,13 +1,9 @@
-import about_img from './about_img.png'
-import bin_icon from './bin_icon.png'
-import cart_icon from './cart_icon.png'
-import contact_img from './contact_img.png'
-import cross_icon from './cross_icon.png'
-import dropdown_icon from './dropdown_icon.png'
-import exchange_icon from './exchange_icon.png'
-import hero_img from './hero_img.png'
+// import about_img from './about_img.png'
+
+import bin_icon from './bin.svg'
+import cart_icon from './cart.svg'
+import cross_icon from './cross.svg'
 import logo from './logo.png'
-import menu_icon from './menu_icon.png'
 import p_img1 from './p_img1.png'
 import p_img10_1 from './p_img10_1.png';
 import p_img10_2 from './p_img10_2.png';
@@ -70,34 +66,92 @@ import p_img7_1 from './p_img7_1.png';
 import p_img8_1 from './p_img8_1.png';
 import p_img8_2 from './p_img8_2.png';
 import p_img9_1 from './p_img9_1.png';
-import profile_icon from './profile_icon.png'
-import quality_icon from './quality_icon.png'
 import razorpay_logo from './razorpay_logo.png'
-import search_icon from './search_icon.png'
-import star_dull_icon from './star_dull_icon.png'
-import star_icon from './star_icon.png'
 import stripe_logo from './stripe_logo.png'
-import support_img from './support_img.png'
+
+// import contact_img from './contact_img.png'
+
+// import dropdown_icon from './dropdown_icon.png'
+// import exchange_icon from './exchange_icon.png'
+// import hero_img from './hero_img.png'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import menu_icon from './menu_icon.png'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import profile_icon from './profile_icon.png'
+// import quality_icon from './quality_icon.png'
+
+// import search_icon from './search_icon.png'
+// import star_dull_icon from './star_dull_icon.png'
+// import star_icon from './star_icon.png'
+
+// import support_img from './support_img.png'
 
 export const assets = {
     logo,
-    hero_img,
+    // hero_img,
     cart_icon,
-    dropdown_icon,
-    exchange_icon,
-    profile_icon,
-    quality_icon,
-    search_icon,
-    star_dull_icon,
-    star_icon,
+    // dropdown_icon,
+    // exchange_icon,
+    // profile_icon,
+    // quality_icon,
+    // search_icon,
+    // star_dull_icon,
+    // star_icon,
     bin_icon,
-    support_img,
-    menu_icon,
-    about_img,
-    contact_img,
+    // support_img,
+    // menu_icon,
+    // about_img,
+    // contact_img,
     razorpay_logo,
     stripe_logo,
-    cross_icon
+     cross_icon
 }
 
 export const products = [
