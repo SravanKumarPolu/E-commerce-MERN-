ff29538f8edfc3621bb079548a4775fb9be07b8a
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2024-12-25 21:15:08 +0530

    need images added

---
+++ frontend/src/assets/assets.ts
@@ -0,0 +1,461 @@
+import about_img from './about_img.png'
+import bin_icon from './bin_icon.png'
+import cart_icon from './cart_icon.png'
+import contact_img from './contact_img.png'
+import cross_icon from './cross_icon.png'
+import dropdown_icon from './dropdown_icon.png'
+import exchange_icon from './exchange_icon.png'
+import hero_img from './hero_img.png'
+import logo from './logo.png'
+import menu_icon from './menu_icon.png'
+import p_img1 from './p_img1.png'
+import p_img10_1 from './p_img10_1.png'
+import p_img11_1 from './p_img11_1.png'
+import p_img12_1 from './p_img12_1.png'
+import p_img13_1 from './p_img13_1.png'
+import p_img14_1 from './p_img14_1.png';
+import p_img15_1 from './p_img15_1.png';
+import p_img16_1 from './p_img16_1.png';
+import p_img17_1 from './p_img17_1.png';
+import p_img18_1 from './p_img18_1.png';
+import p_img19_1 from './p_img19_1.png';
+import p_img20_1 from './p_img20_1.png';
+import p_img21_1 from './p_img21_1.png';
+import p_img22_1 from './p_img22_1.png';
+import p_img23_1 from './p_img23_1.png';
+import p_img24_1 from './p_img24_1.png';
+import p_img25_1 from './p_img25_1.png';
+import p_img26_1 from './p_img26_1.png';
+import p_img27_1 from './p_img27_1.png';
+import p_img28_1 from './p_img28_1.png';
+import p_img29_1 from './p_img29_1.png';
+import p_img2_1 from './p_img2_1.png'
+import p_img2_2 from './p_img2_2.png'
+import p_img2_3 from './p_img2_3.png'
+import p_img2_4 from './p_img2_4.png'
+import p_img3 from './p_img3.png'
+import p_img30_1 from './p_img30_1.png';
+import p_img4_1 from './p_img4_1.png';
+import p_img5_1 from './p_img5_1.png';
+import p_img6_1 from './p_img6_1.png';
+import p_img7_1 from './p_img7_1.png';
+import p_img8_1 from './p_img8_1.png';
+import p_img9_1 from './p_img9_1.png';
+import profile_icon from './profile_icon.png'
+import quality_icon from './quality_icon.png'
+import razorpay_logo from './razorpay_logo.png'
+import search_icon from './search_icon.png'
+import star_dull_icon from './star_dull_icon.png'
+import star_icon from './star_icon.png'
+import stripe_logo from './stripe_logo.png'
+import support_img from './support_img.png'
+
+export const assets = {
+    logo,
+    hero_img,
+    cart_icon,
+    dropdown_icon,
+    exchange_icon,
+    profile_icon,
+    quality_icon,
+    search_icon,
+    star_dull_icon,
+    star_icon,
+    bin_icon,
+    support_img,
+    menu_icon,
+    about_img,
+    contact_img,
+    razorpay_logo,
+    stripe_logo,
+    cross_icon
+}
+
+export const products = [
+    {
+        _id: "aaaaa",
+        name: "apple watch series 10 ",
+        description: "The thinnest Apple Watch ever, with our biggest display. With a new wide-angle Always-On OLED Retina display Footnote ",
+        price: 400,
+        image: [p_img1],
+        category: "Watch ",
+        subCategory: "pro",
+        colors: ["Silver", " Gold", "Black ", "white"],
+        date: 1716634345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaab",
+        name: "iPhone 16 Pro",
+        description: "iPhone 16 ProTitanium design with larger 15.9 cm (6.3″) Super Retina XDR display, footnote 1 durable, latest-generation Ceramic Shield, Action button, and USB-C with USB 3 speeds footnote 2",
+        price: 200,
+        image: [p_img2_1, p_img2_2, p_img2_3, p_img2_4],
+        category: "iPhone ",
+        subCategory: "Pro ",
+        colors: ["White", " Desert Titanium", "Black Titanium"],
+        date: 1716621345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaac",
+        name: "iPad Pro 11",
+        description: "Apple Intelligence helps you write, express yourself, and get things done effortlessly while giving you peace of mind with groundbreaking privacy protections.",
+        price: 620,
+        image: [p_img3],
+        category: "iPad",
+        subCategory: " Pro ",
+        colors: ["Silver", "Black ", "White"],
+        date: 1716234545448,
+        bestseller: true
+    },
+    {
+        _id: "aaaad",
+        name: "iPad 5 Plus",
+        description: "The A14 Bionic chip lets you run multiple apps and work smoothly between them",
+        price: 500,
+        image: [p_img4_1],
+        category: "iPad",
+        subCategory: "Plus",
+        colors: ["Silver", "Black ", "White", "Yellow"],
+        date: 1716621345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaae",
+        name: "MacBook Pro 14",
+        description: "Supercharged by M4, M4 Pro or M4 Max, MacBook Pro empowers you to take on the most demanding projects",
+        price: 1530,
+        image: [p_img5_1],
+        category: "Laptops",
+        subCategory: "Pro",
+        colors: ["Silver", "Black ",],
+        date: 1716622345448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaf",
+        name: "MacBook 14 Plus",
+        description: "The powerful 8-core CPU and up to 10-core GPU of the Apple M2 chip keep things running smoothly",
+        price: 1500,
+        image: [p_img6_1],
+        category: "Laptop",
+        subCategory: "Plus",
+        colors: ["Silver", " Gold", "Black ", "white"],
+        date: 1716623423448,
+        bestseller: true
+    },
+    {
+        _id: "aaaag",
+        name: "MacBook Pro 16",
+        description: "The 16.2-inch Liquid Retina XDR display features 1,600 nits peak brightness and up to 1,000 nits sustained brightness, 1,000,000:1 contrast. With nano‑texture option",
+        price: 1699,
+        image: [p_img7_1],
+        category: "Laptop",
+        subCategory: "Pro",
+        colors: ["Silver", "Black ", "white"],
+        date: 1716621542448,
+        bestseller: false
+    },
+    {
+        _id: "aaaah",
+        name: "iPhone 16 plus",
+        description: "15.5 cm (6.1″) aerospace-grade aluminium design footnote 1 with durable, latest-generation Ceramic Shield front, Action button and USB-C",
+        price: 900,
+        image: [p_img8_1],
+        category: "iPhone",
+        subCategory: "Plus",
+        colors: [" Gold", "Black ", "White", "Teal", "Pink"],
+
+        date: 1716622345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaai",
+        name: "MacBook 16 ",
+        description: "Built for Apple Intelligence. Personal, private, powerful.",
+        price: 1620,
+        image: [p_img9_1],
+        category: "Laptop",
+        subCategory: "Plus",
+        colors: [" Gold", "Black ", "white"],
+        date: 1716621235448,
+        bestseller: false
+    },
+    {
+        _id: "aaaaj",
+        name: "Apple Watch Ultra 1",
+        description: "With Family Setup, your kids can use Apple Watch,  footnote  § even if they don’t have their own iPhone",
+        price: 380,
+        image: [p_img10_1],
+        category: "Watch",
+        subCategory: "Ultra",
+        colors: [" Gold", "Black ", "Yellow", "Teal", "Pink"],
+        date: 1716622235448,
+        bestseller: false
+    },
+    {
+        _id: "aaaak",
+        name: "iPhone 15 ",
+        description: "15.5 cm durable colour-infused glass and aluminium design footnote ◊ with Ceramic Shield front",
+        price: 890,
+        image: [p_img11_1],
+        category: "iPhone",
+        subCategory: "Plus",
+        colors: ["White", "Teal", "Ultramarine"],
+        date: 1716623345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaal",
+        name: "Apple Watch Ultra 2",
+        description: "The ultimate sports and adventure watch has a rugged 49mm titanium case with 100-metre water resistance  footnote  § and IP6X dust resistance.  footnote  ° Now in black",
+        price: 360,
+        image: [p_img12_1],
+        category: "Watch",
+        subCategory: "Ultra",
+        colors: ["White", "Teal"],
+        date: 1716624445448,
+        bestseller: false
+    },
+    {
+        _id: "aaaam",
+        name: "iPhone 15 Plus",
+        description: ".A16 Bionic powers computational photography and smooth gaming performance with great efficiency for all-day battery life.",
+        price: 920,
+        image: [p_img13_1],
+        category: "iPhone",
+        subCategory: "Plus",
+        colors: ["White", "Teal", "Pink", "Ultramarine"],
+        date: 1716625545448,
+        bestseller: false
+    },
+    {
+        _id: "aaaan",
+        name: "MacBook Pro 13",
+        description: "MacBook Pro 2020 (Touch Bar, Four Thunderbolt 3 ports) A2251 (Intel Core i7 13 Inch)",
+        price: 160,
+        image: [p_img14_1],
+        category: "Laptop",
+        subCategory: "Pro",
+        colors: ["White", "Teal", "Black", "Ultramarine"],
+        date: 1716626645448,
+        bestseller: false
+    },
+    {
+        _id: "aaaao",
+        name: "Apple MacBook Ultra 13.3",
+        description: "Apple MacBook Ultra 13.3 - Intel Core i5 - 8GB RAM - 256GB SSD Silver",
+        price: 1140,
+        image: [p_img15_1],
+        category: "Laptop",
+        subCategory: "Ultra",
+        colors: ["White", "Teal", "Black", "Ultramarine"],
+        date: 1716627745448,
+        bestseller: false
+    },
+    {
+        _id: "aaaap",
+        name: "Apple Watch Series 9",
+        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
+        price: 400,
+        image: [p_img16_1],
+        category: "Watch",
+        subCategory: "Plus",
+        colors: ["White", "Teal", "Pink",],
+        date: 1716628845448,
+        bestseller: false
+    },
+    {
+        _id: "aaaaq",
+        name: "Apple Watch Series 7",
+        description: "Apple Watch Series 7 Case Sport Band.",
+        price: 150,
+        image: [p_img17_1],
+        category: "Watch",
+        subCategory: "Pulse",
+        colors: ["Black", "White", "Teal", "Pink", "Ultramarine"],
+        date: 1716629945448,
+        bestseller: false
+    },
+    {
+        _id: "aaaar",
+        name: "Apple AirPods Pro",
+        description: "Redesigned for exceptional fit and audio performance. The powerful H2 chip delivers clearer calls with Voice Isolation and a new, hands-free way to interact with Siri.",
+        price: 280,
+        image: [p_img18_1],
+        category: "AirPods",
+        subCategory: "Pro",
+        colors: ["White", "Teal", "Pink",],
+        date: 1716631045448,
+        bestseller: false
+    },
+    {
+        _id: "aaaas",
+        name: "Apple AirPods Plus",
+        description: "USB-C charging and up to 30 hours of listening time using the case. Footnote ¹⁴ And Find My to help you locate your AirPods 4",
+        price: 260,
+        image: [p_img19_1],
+        category: "Kids",
+        subCategory: "Plus",
+        colors: ["White", "Teal",],
+        date: 1716632145448,
+        bestseller: false
+    },
+    {
+        _id: "aaaat",
+        name: "Apple Watch Ultra 1.3",
+        description: "Apple Watch Ultra 1.3 GPS 41mm Midnight Aluminium Case with Midnight Sport Band.",
+        price: 370,
+        image: [p_img20_1],
+        category: "Watch",
+        subCategory: "Ultra",
+        colors: ["Black", "White", "Teal", "Pink",],
+        date: 1716633245448,
+        bestseller: false
+    },
+    {
+        _id: "aaaau",
+        name: "	2024 Apple iMac with Retina 5K Display",
+        description: "iMac performance is at an all-time high with tenth-generation Intel Core processors, powerful AMD Radeon Pro 5000 series graphics, ultrafast SSD storage, Thunderbolt 3 (USB-C) connectivity",
+        price: 470,
+        image: [p_img21_1],
+        category: "TV",
+        subCategory: "Pro",
+        colors: ["Black", "White", "Teal",],
+        date: 1716634345448,
+        bestseller: false
+    },
+    {
+        _id: "aaaav",
+        name: "Apple iMac (27-inch, 8GB RAM, 1TB Storage) ",
+        description: "iMac performance is at an all-time high with tenth-generation Intel Core processors, powerful AMD Radeon Pro 5000 series graphics, ultrafast SSD storage, Thunderbolt 3 (USB-C) connectivity.",
+        price: 410,
+        image: [p_img22_1],
+        category: "TV",
+        subCategory: "Pluse",
+        colors: ["Black", "White", "Teal",],
+        date: 1716635445448,
+        bestseller: false
+    },
+    {
+        _id: "aaaaw",
+        name: "Apple iMac 27",
+        description: "Imac performance is at an all-time high with faster Intel Core processors, powerful Radeon Pro 500-Series graphics, and stunning Retina 4K and 5K displays.",
+        price: 180,
+        image: [p_img23_1],
+        category: "TV",
+        subCategory: "Ultra",
+        colors: ["Black", "White", "Teal"],
+        date: 1716636545448,
+        bestseller: false
+    },
+    {
+        _id: "aaaax",
+        name: "iPhone 14",
+        description: "A Pro-level Main camera and advanced image processing for sensational shots in all kinds of light.",
+        price: 710,
+        image: [p_img24_1],
+        category: "iPhone",
+        subCategory: "Plus",
+        colors: ["White", "Teal", "Black", "Ultramarine"],
+        date: 1716637645448,
+        bestseller: false
+    },
+    {
+        _id: "aaaay",
+        name: "iPhone 14 Pro",
+        description: "Superfast 5G. With SmartData mode, iPhone downshifts automatically to save power when you don’t need all that speed.",
+        price: 679,
+        image: [p_img25_1],
+        category: "iPhone",
+        subCategory: "Pro",
+        colors: ["White", "Teal", "Black", "Ultramarine", "Pink"],
+        date: 1716638745448,
+        bestseller: true
+    },
+    {
+        _id: "aaaaz",
+        name: "iPhone 14 Ultra",
+        description: "Superfast 5G. With SmartData mode, iPhone downshifts automatically to save power when you don’t need all that speed.",
+        price: 220,
+        image: [p_img26_1],
+        category: "iPhone",
+        subCategory: "Ultra",
+        colors: ["White", "Teal", "Black", "Ultramarine", "Pink"],
+        date: 1716639845448,
+        bestseller: false
+    },
+    {
+        _id: "aaaba",
+        name: "apple airpods pro 4",
+        description: " AirPods Pro 2 Pro-level Active Noise Cancellation and advancements in hearing health",
+        price: 200,
+        image: [p_img27_1],
+        category: "Airpods",
+        subCategory: "Pro",
+        colors: ["White", "Teal", "Black", "Ultramarine", "Pink"],
+        date: 1716640945448,
+        bestseller: false
+    },
+    {
+        _id: "aaabb",
+        name: "apple airpods Plus 4",
+        description: "AirPods 4 Active Noise Cancellation The next evolution of sound, comfort and noise control.",
+        price: 230,
+        image: [p_img28_1],
+        category: "Airpods",
+        subCategory: "Plus",
+        colors: ["White", "Teal", "Black", "Ultramarine", "Pink"],
+        date: 1716642045448,
+        bestseller: false
+    },
+    {
+        _id: "aaabc",
+        name: "apple airpods Ultra 4",
+        description: "AirPods Ultra 4 The next evolution of sound and comfort.",
+        price: 210,
+        image: [p_img29_1],
+        category: "Airpods",
+        subCategory: "Ultra",
+        colors: ["White", "Teal", "Black", "Ultramarine", "Pink"],
+        date: 1716643145448,
+        bestseller: false
+    },
+    {
+        _id: "aaabd",
+        name: "Watch Series pro 10",
+        description: "APPLE WATCH SERIES 10 Jet Black Aluminium Case with Sport Loop.",
+        price: 250,
+        image: [p_img30_1],
+        category: "Watch",
+        subCategory: "Pro",
+        colors: ["White", "Teal", "Black", "Ultramarine", "Pink"],
+        date: 1716644245448,
+        bestseller: false
+    },
+    {
+        _id: "aaabe",
+        name: "Watch Series Plus 10",
+        description: "APPLE WATCH SE  Aluminium Case with Sport Loop",
+        price: 249,
+        image: [p_img31_1],
+        category: "Watch",
+        subCategory: "Plus",
+        colors: ["Silver",],
+        date: 1716644245448,
+        bestseller: false
+    },
+    {
+        _id: "aaabf",
+        name: "Watch Series Ultra 10",
+        description: "APPLE WATCH ULTRA 2 Natural Titanium Case with Alpine Loop.",
+        price: 244,
+        image: [p_img32_1],
+        category: "Watch",
+        subCategory: "Ultra",
+        colors: ["White", "Teal", "Black", "Ultramarine", "Pink"],
+        date: 1716644245448,
+        bestseller: false
+    },
+
+]
