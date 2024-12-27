4c52728f65d6284df2a818a37ba2b97281578edd
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2024-12-27 16:01:56 +0530

    Update About.tsx

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -17,7 +17,7 @@ const About = () => {
             At the Apple Store, we invite you to explore our latest range of products, from iPhones to Macs, designed to fit your lifestyle. Whether you're upgrading your current device or discovering new innovations, we have everything you need. Follow us for the latest updates, tech tips, and exclusive offers. Experience the future of technology today with Apple.
           </p>
           <p className="leading-relaxed text-lg">
-            Our mission at Apple is clear: to create the best products and deliver exceptional service that empowers our customers. We are committed to innovation, sustainability, and making technology accessible for everyone. Join us in shaping the future of tech with groundbreaking products and services.
+            <span className="font-raleway font-semibold">Our mission at Apple is clear :</span>  to create the best products and deliver exceptional service that empowers our customers. We are committed to innovation, sustainability, and making technology accessible for everyone. Join us in shaping the future of tech with groundbreaking products and services.
           </p>
           <b className="text-gray-800 font-raleway text-xl">Our Mission</b>
           <p className="leading-relaxed text-lg">
@@ -34,15 +34,15 @@ const About = () => {
       {/* Benefits Section */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-20 text-sm text-gray-600">
         <div className="border px-10 py-8 sm:py-20 flex flex-col gap-5 rounded-lg shadow-md hover:shadow-xl transition-all duration-300">
-          <b className="text-xl font-semibold">Quality Assurance</b>
+          <h2 className="text-xl font-raleway font-semibold">Quality Assurance</h2>
           <p>Each Apple product undergoes rigorous testing to ensure it meets the highest standards of quality, durability, and performance.</p>
         </div>
         <div className="border px-10 py-8 sm:py-20 flex flex-col gap-5 rounded-lg shadow-md hover:shadow-xl transition-all duration-300">
-          <b className="text-xl font-semibold">Affordable Prices</b>
+          <h2 className="text-xl font-raleway font-semibold">Affordable Prices</h2>
           <p>We believe that innovation should be accessible to everyone, which is why we offer competitive pricing on all Apple devices and services.</p>
         </div>
         <div className="border px-10 py-8 sm:py-20 flex flex-col gap-5 rounded-lg shadow-md hover:shadow-xl transition-all duration-300">
-          <b className="text-xl font-semibold">Exceptional Service</b>
+          <h2 className="text-xl font-raleway font-semibold">Exceptional Service</h2>
           <p>From personalized support in-store to seamless online experiences, Apple’s customer service is always ready to help you get the most out of your device.</p>
         </div>
       </div>
