d85fad425aa5f0c8e17f55a8882fcd4ace0b6a6d
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2024-12-27 13:51:19 +0530

    pages

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,37 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'], //NavBar
+
+        montserrat: ['Montserrat', 'sans-serif'], // For headlines
+        openSans: ['Open Sans', 'sans-serif'],    // For body text
+        workSans: ['Work Sans', 'sans-serif'],    // For Button font
+
+        poppins: ['Poppins', 'sans-serif'],       // For headlines
+        roboto: ['Roboto', 'sans-serif'],         // For body text
+        barlow: ['Barlow', 'sans-serif'],         // for Button font
+
+        playfair: ['Playfair Display', 'serif'],  // For headlines
+        lora: ['Lora', 'serif'],                  // For body text
+        nunito: ['Nunito', 'sans-serif'],         // for Button font
+
+        oswald: ['Oswald', 'sans-serif'],         // For headlines
+        inter: ['Inter', 'sans-serif'],           // For body text
+        fjallaOne: ['Fjalla One', 'sans-serif'],  // for Button font
+
+        raleway: ['Raleway', 'sans-serif'],       // For headlines
+        notoSans: ['Noto Sans', 'sans-serif'],    // For body text
+        workSans: ['Work Sans', 'sans-serif'],    // For Button font
+
+
+
+
+
+
+      },
+    },
   },
   plugins: [
     require('daisyui'),
