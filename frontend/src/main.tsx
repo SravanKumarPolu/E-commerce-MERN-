f00e1ab732c2fa1d35113fc90a141374e4174a29
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-02-11 22:24:18 +0530

    Relatedproduct created

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,15 +2,16 @@ import './index.css';
 
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import ShopContextProvider from './context/ShopContext.tsx';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-
+      <ShopContextProvider>
       <App />
-
+      </ShopContextProvider>
 
     </BrowserRouter>
   </StrictMode>,
