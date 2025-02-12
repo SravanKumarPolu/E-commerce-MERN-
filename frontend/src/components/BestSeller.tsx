64f70ddf57b291afa4283cad9fc17b0068e31a62
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-02-12 22:02:41 +0530

    Update BestSeller.tsx

--- frontend/src/components/BestSeller.tsx
+++ frontend/src/components/BestSeller.tsx
@@ -35,7 +35,7 @@ const BestSeller: React.FC = () => {
       </div>
 
       {/* Product Grid */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 mt-8">
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
         {bestSeller.map((item) => (
           <div
             key={item._id}
