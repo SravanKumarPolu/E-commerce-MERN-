66b91cb70f8f4ef0a9ffd28a9ec1dd26af04ce9f
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-02-20 23:58:55 +0530

    product page implementing

--- frontend/src/pages/Product.tsx
+++ frontend/src/pages/Product.tsx
@@ -1,3 +1,7 @@
+import { useContext, useEffect, useState } from "react";
+
+import { ShopContext } from "../context/ShopContext";
+import { useParams } from "react-router-dom";
 
 interface Product {
   _id: string;
@@ -14,12 +18,29 @@ interface Product {
 }
 
 const Product: React.FC = () => {
+  const { productId } = useParams();
+  const { products } = useContext(ShopContext)
+  const [productData, setProductData] = useState<Product | null>(null);
+  const [selectedImage, setSelectedImage] = useState<string>("");
+  const filterProductsData = async () => {
+    products.map((item) => {
+      if (item.id === productId) {
+        setProductData(item)
+        setImage(item.image[0])
+        console.log()
+        return null
+      }
+    })
+  }
+  useEffect(() => {
+    filterProductsData()
+  }, [productId])
 
-  return (
+  return productData ? (
     <div className="border-t-2 pt-10 transition-opacity ease-in duration-500">
       products
     </div>
-  );
+  ) : <div className="opacity-0"></div>;
 };
 
 export default Product;
