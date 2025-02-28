ba35fb42491ea06daf7b271efbe02b6f57af62d8
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-02-28 20:29:28 +0530

    login page created

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -62,11 +62,11 @@ const NavBar = () => {
           onClick={() => setShowSearch(true)}
           src={assets.search_icon} className="w-6 cursor-pointer " alt="Search" />
         <div className='group relative'>
-
+          <Link to={'/login'} >
           <img
 
             src={assets.profile_icon} className="w-6 cursor-pointer " alt='Profile' />
-
+          </Link>
           <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
             <div className='flex flex-col gap-2 w-36 px-5 bg-slate-100 text-gray-500 rounded '>
               <p className='cursor-pointer hover:text-black'>My Profile</p>
