ab60375e5b118483dda7e674e9c0401619bdefa3
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-03-08 23:31:31 +0530

    Create userRoute.js

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,9 @@
+import { loginAdmin, loginUser, registerUser } from '../controllers/useController';
+
+import express from 'express'
+
+const userRouter=express.Router();
+ userRouter.post('/login',loginUser)
+ userRouter.post('/register',registerUser)
+ userRouter.post('/admin',loginAdmin)
+ export  default userRouter;
