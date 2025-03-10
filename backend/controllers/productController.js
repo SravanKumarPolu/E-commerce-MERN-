7ea7d18fc943f31fb2b3fa617856a72854ee2635
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-03-10 14:22:38 +0530

    productRoute and ProductController  created

---
+++ backend/controllers/productController.js
@@ -0,0 +1,16 @@
+
+const addProduct=async(res,req)=>{
+
+}
+const singleProduct=async(res,req)=>{
+
+}
+const removeProduct=async(res,req)=>{
+
+}
+const listProduct=async(res,req)=>{
+  
+}
+
+
+export { addProduct, singleProduct, removeProduct, listProduct }
---
+++ backend/routes/productRoute.js
@@ -0,0 +1,9 @@
+import { addProduct, listProduct, removeProduct, singleProduct } from '../controllers/productController';
+
+import express from 'express'
+
+const productRouter=express.Router();
+productRouter.post('/add',addProduct)
+productRouter.post('/single',singleProduct)
+productRouter.post('/remove',removeProduct)
+productRouter.post('/list',listProduct)
