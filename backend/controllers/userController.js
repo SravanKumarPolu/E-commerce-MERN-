57efc7024516e736c7424ca3f91a669858dfcec2
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-03-11 20:50:50 +0530

    updated Route for user register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,49 @@
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js"
+import validator from 'validator'
 
-
-const loginUser=async(res,req)=>{
+const createToken=(id)=>{
+  return jwt.sign({id},process.env.JWT_SECRET)
+}
+const loginUser=async(req,res)=>{
 
 }
-const registerUser=async(res,req)=>{
+//Route for user register
+const registerUser=async(req,res)=>{
+try {
+  const {name,email,password}=req.body
+  //checking user already exists or not
+  const exist=await userModel.findOne({email})
+
+  if(exist){
+  return res.json({success:false,message:"Already user exists"})
+  }
+  //validating email formate and strong password
+  if(!validator.isEmail(email)){
+    return res.json({success:false,message:"Please enter a valid Email"})
+  }
+  if(password.length<8){
+    return res.json({success:"false",message:"Please enter  a strong Password"})
+  }
+  //hashing user password
+  const salt= await bcrypt.genSalt(10);
+  const hashedPassword=await bcrypt.hash(password,salt)
+  const newUser= new userModel({
+    name,email,password:hashedPassword
+  })
+  const user=  await  newUser.save();
+  const token=createToken(user._id)
+  res.json({success:true,token})
+
+} catch (error) {
+  console.log(error);
+  res.json({success:false,message:error.message})
+}
+}
+
+
+const adminLogin=async(req,res)=>{
 
 }
-const loginAdmin=async(res,req)=>{
-
-}
-export {loginAdmin,loginUser,registerUser}
+export {adminLogin,loginUser,registerUser}
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,9 @@
-import { loginAdmin, loginUser, registerUser } from '../controllers/userController.js';
+import { adminLogin, loginUser, registerUser } from '../controllers/userController.js';
 
 import express from 'express'
 
 const userRouter=express.Router();
  userRouter.post('/login',loginUser)
  userRouter.post('/register',registerUser)
- userRouter.post('/admin',loginAdmin)
+ userRouter.post('/admin',adminLogin)
  export  default userRouter;
