173cf59beefee4ce647871561e0ed454c920bdb8
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-03-19 12:23:44 +0530

    updated

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,10 +1,10 @@
 import multer from 'multer'
 
-const storge=multer.diskStorage({
-  filename:function(req,file,callback){
-    callback(null,file.originalname)
+const storage = multer.diskStorage({
+  filename: function (req, file, callback) {
+    callback(null, file.originalname)
   }
 })
-const upload=multer({storge});
-export default upload
 
+const upload = multer({ storage })
+export default upload;
