1e24dc67bd41394722e09f2a3a1aa747df8dcb4e
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-03-19 12:23:22 +0530

    Update productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,4 @@
+import { v2 as cloudinary } from 'cloudinary'
 
 const addProduct=async(req,res)=>{
 try {
@@ -6,9 +7,16 @@ try {
   const image2 = req.files.image2 && req.files.image2[0];
   const image3 = req.files.image3 && req.files.image3[0];
   const image4 = req.files.image4 && req.files.image4[0];
+  const images=[image1,image2,image3,image4].filter((item)=>item !== undefined)
+  let imagesUrl = await Promise.all(
+    images.map(async (item) => {
+      let result = await cloudinary.uploader.upload(item.path, { resource_type: 'image' });
+      return result.secure_url
+    })
+  )
 console.log(name, description, price, category, subCategory, color, bestseller )
-console.log(image1,image2,image3,image4)
-res.json({})
+console.log(imagesUrl)
+res.json({ success: true, message: "Product Added" })
 } catch (error) {
   console.log(error)
   res.json({success:false,message:error.message})
