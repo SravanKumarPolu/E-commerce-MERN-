47007553c69008c1d1f388e1ae3e13045183fb4d
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-03-25 13:35:30 +0530

    styles updated

--- admin/src/components/Sidebar.tsx
+++ admin/src/components/Sidebar.tsx
@@ -4,16 +4,17 @@ import { NavLink } from 'react-router-dom';
 
 function Sidebar() {
   const linkClasses = ({ isActive }: { isActive: boolean }) =>
-    `flex items-center gap-3 px-4 py-3 rounded-lg transition-colors duration-200 ${isActive
-      ? 'bg-gray-200 text-pink-900 font-semibold'
+    `flex items-center gap-3 border border-gray-300 shadow-md border-r-0 px-3 py-2 rounded-l transition-colors duration-200 ${isActive
+      ? 'bg-gray-200 text-gray-900 font-semibold'
       : 'hover:bg-gray-100 text-gray-700'
     }`;
 
   return (
     <aside className="w-[18%] min-h-screen bg-white shadow-md  hidden md:block">
-      <nav className="menu p-4 text-sm">
-        <ul className="space-y-2">
+      <nav className="flex flex-col gap-4 pt-6 pl-[20%] text-[14px]'">
+        <ul className="space-y-1">
           <li>
+
             <NavLink to="/add" className={linkClasses}>
               <AddIcon />
               <span className="hidden md:block">Add Items</span>
