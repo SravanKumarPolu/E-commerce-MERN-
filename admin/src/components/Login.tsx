4d41ef20c856eaf5928089f8de25267d8c368c19
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-03-25 14:39:40 +0530

    Update Login.tsx

--- admin/src/components/Login.tsx
+++ admin/src/components/Login.tsx
@@ -1,12 +1,27 @@
-function Login() {
+import { toast } from "react-toastify";
+import { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      console.log(email, password)
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-100 px-4">
       <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-8">
         <h1 className="text-3xl font-bold mb-6 text-center text-gray-800">Admin Panel</h1>
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={onSubmitHandler}>
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">Email Address</label>
             <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               type="email"
               placeholder="you@example.com"
               required
@@ -16,6 +31,8 @@ function Login() {
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">Password</label>
             <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               type="password"
               placeholder="Enter your password"
               required
