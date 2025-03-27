4b7c471b61b2f2e355326483710df7761b64d680
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-03-27 14:18:00 +0530

    Update add.tsx

--- admin/src/pages/add.tsx
+++ admin/src/pages/add.tsx
@@ -3,6 +3,18 @@ interface AddProps {
   token: string;
 }
 const Add: React.FC<AddProps> = () => {
+  const [image1, setImage1] = useState(false)
+  const [image2, setImage2] = useState(false);
+  const [image3, setImage3] = useState(false);
+  const [image4, setImage4] = useState(false);
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [price, setPrice] = useState('')
+  const [category, setCategory] = useState("iPhone")
+  const [subCategory, setSubCategory] = useState("Pro")
+  const [bestseller, setBestseller] = useState(false)
+  const [color, setColor] = useState<string[]>([]);
+
   return (
     <>
       <form className='flex flex-col w-full items-start gap3'>
@@ -10,35 +22,35 @@ const Add: React.FC<AddProps> = () => {
           <p className='mb-2'>Upload Image</p>
           <div className='flex gap-2'>
             <label htmlFor='image1' >
-              <img className='w-20' src={assets.upload_area} alt='' />
-              <input type='file' id='image1' hidden />
+              <img className='w-20' src={!image1 ? assets.upload_area : URL.createObjectURL(image1)} alt='' />
+              <input onChange={(e) => setImage1(e.target.files[0])} type='file' id='image1' hidden />
             </label>
             <label htmlFor='image2' >
-              <img className='w-20' src={assets.upload_area} alt='' />
-              <input type='file' id='image2' hidden />
+              <img className='w-20' src={!image2 ? assets.upload_area : URL.createObjectURL(image2)} alt='' />
+              <input onChange={(e) => setImage2(e.target.files[0])} type='file' id='image2' hidden />
             </label>
             <label htmlFor='image3' >
-              <img className='w-20' src={assets.upload_area} alt='' />
-              <input type='file' id='image3' hidden />
+              <img className='w-20' src={!image3 ? assets.upload_area : URL.createObjectURL(image3)} alt='' />
+              <input onChange={(e) => setImage3(e.target.files[0])} type='file' id='image3' hidden />
             </label>
             <label htmlFor='image4' >
-              <img className='w-20' src={assets.upload_area} alt='' />
-              <input type='file' id='image4' hidden />
+              <img className='w-20' src={!image4 ? assets.upload_area : URL.createObjectURL(image4)} alt='' />
+              <input onChange={(e) => setImage4(e.target.files[0])} type='file' id='image4' hidden />
             </label>
           </div>
         </div>
         <div className='w-full'>
           <p className='mb-2'>Product name</p>
-          <input className='w-full max-w-[500px] px-3 py-2 ' type='text' placeholder='Type here' required />
+          <input onChange={(e) => setName(e.target.value)} value={name} className='w-full max-w-[500px] px-3 py-2 ' type='text' placeholder='Type here' required />
         </div>
         <div className='w-full'>
           <p className='mb-2'>Product description</p>
-          <input className='w-full max-w-[500px] px-3 py-2 ' type='text' placeholder='Write content here' required />
+          <input onChange={(e) => setDescription(e.target.value)} value={description} className='w-full max-w-[500px] px-3 py-2 ' type='text' placeholder='Write content here' required />
         </div>
         <div className=" flex flex-col sm:flex-row gap-2 w-full sm:gap-8">
           <div className="">
             <p className='mb-2'>Product category</p>
-            <select className="w-full px-3 py-2" name="" id="">
+            <select onChange={(e) => setCategory(e.target.value)} className="w-full px-3 py-2" name="" id="">
               <option value="Watch">Watch</option>
               <option value="iPad">iPad</option>
               <option value="iPhone">iPhone</option>
@@ -50,7 +62,7 @@ const Add: React.FC<AddProps> = () => {
           </div>
           <div className="">
             <p className='mb-2'> Sub category</p>
-            <select className="w-full px-3 py-2" name="" id="">
+            <select onChange={(e) => setSubCategory(e.target.value)} className="w-full px-3 py-2" name="" id="">
               <option value="Plus">Plus</option>
               <option value="Pro">Pro</option>
               <option value="Ultra">Ultra</option>
