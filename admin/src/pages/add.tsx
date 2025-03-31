222969ef552e8483cc70e7152c9e06c2791b3e5a
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-03-31 23:42:06 +0530

    Update add.tsx

--- admin/src/pages/add.tsx
+++ admin/src/pages/add.tsx
@@ -1,7 +1,12 @@
 import { assets } from '../assets/assets';
+import axios from 'axios';
+import { backendUrl } from '../App';
 import { useState } from 'react';
 
-const Add = () => {
+interface AddProps {
+  token: string;
+}
+const Add: React.FC<AddProps> = ({ token }) => {
   const [image1, setImage1] = useState<File | null>(null);
   const [image2, setImage2] = useState<File | null>(null);
   const [image3, setImage3] = useState<File | null>(null);
@@ -9,16 +14,37 @@ const Add = () => {
 
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
-
+  const [price, setPrice] = useState('');
   const [category, setCategory] = useState('iPhone');
   const [subCategory, setSubCategory] = useState('Pro');
   const [bestseller, setBestseller] = useState(false);
   const [color, setColor] = useState<string[]>([]);
 
   const colorOptions = ["Black", "Black Titanium", "Desert Titanium", "Gold", "Pink", "Silver", "Teal", "Ultramarine", "White", "Yellow"];
+  const onSubmitHandler = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData()
+      formData.append("name", name)
+      formData.append("description", description)
+      formData.append("price", price)
+      formData.append("category", category)
+      formData.append("subCategory", subCategory)
+      formData.append("bestseller", JSON.stringify(bestseller))
+      formData.append("color", JSON.stringify(color))
+      if (image1) formData.append('image1', image1);
+      if (image2) formData.append('image2', image2);
+      if (image3) formData.append('image3', image3);
+      if (image4) formData.append('image4', image4);
+      const response = await axios.post(backendUrl + "/api/product/add", formData, { headers: { token }, })
+      console.log(response)
+    } catch (error) {
+      console.log(error)
 
+    }
+  }
   return (
-    <form className="max-w-5xl w-full mx-auto mt-10 px-4 sm:px-6 lg:px-8 space-y-8 text-gray-700">
+    <form onSubmit={onSubmitHandler} className="max-w-5xl w-full mx-auto mt-10 px-4 sm:px-6 lg:px-8 space-y-8 text-gray-700">
       {/* Title */}
       <h2 className="text-2xl font-bold mb-2">Add New Product</h2>
 
@@ -112,7 +138,8 @@ const Add = () => {
           <input
             type="number"
             placeholder="100"
-
+            onChange={(e) => setPrice(e.target.value)}
+            value={price}
             className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none  hover:shadow-md focus:shadow-md"
           />
         </div>
@@ -149,6 +176,7 @@ const Add = () => {
           id="bestseller"
           checked={bestseller}
           onChange={() => setBestseller((prev) => !prev)}
+
           className="accent-black"
         />
         <label htmlFor="bestseller" className="text-sm cursor-pointer">
