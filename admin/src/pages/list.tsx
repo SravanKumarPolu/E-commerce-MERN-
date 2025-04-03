5e3a93f28cdd9936abfe32ddc3c82b8d599fb435
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-04-03 15:04:47 +0530

    Update list.tsx

--- admin/src/pages/list.tsx
+++ admin/src/pages/list.tsx
@@ -6,7 +6,20 @@ interface ListProps {
 
 const List: React.FC<ListProps> = () => {
   return (
-    <div>list</div>
+    <>
+      <p className="mb-2">All Products List</p>
+      <div className="flex flex-col gap-2">
+        {/* List table Title */}
+        <div className="hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b className="text-center">Action</b>
+        </div>
+
+      </div>
+    </>
   )
 }
 
