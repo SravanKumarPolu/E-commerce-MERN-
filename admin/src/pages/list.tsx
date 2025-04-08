b92dde1639c05582375fe03b8bc9f9ddee874c76
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-04-08 22:39:48 +0530

    Update list.tsx

--- admin/src/pages/list.tsx
+++ admin/src/pages/list.tsx
@@ -1,10 +1,45 @@
+import { backendUrl, currency } from "../App";
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+import { toast } from "react-toastify";
+
 interface ListProps {
   token: string;
 }
 
-
+interface Product {
+  _id: string;
+  name: string;
+  category: string;
+  price: number;
+  image: string[];
+}
 
 const List: React.FC<ListProps> = () => {
+  const [list, setList] = useState<Product[]>([]);
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(backendUrl + '/api/product/list');
+
+      if (response.data.success) {
+        setList(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      if (error instanceof Error) {
+        toast.error(error.message);
+      } else {
+        toast.error("Something went wrong!");
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
   return (
     <>
       <p className="mb-2">All Products List</p>
@@ -17,7 +52,22 @@ const List: React.FC<ListProps> = () => {
           <b>Price</b>
           <b className="text-center">Action</b>
         </div>
+        {/* Product List */}
+        {list.map((item, index) => (
+          <div
+            className="hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border text-sm"
+            key={index}
+          >
+            <img className="w-12" src={item.image[0]} alt={item.name} />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>
+              {currency} {item.price}
+            </p>
 
+
+          </div>
+        ))}
       </div>
     </>
   )
