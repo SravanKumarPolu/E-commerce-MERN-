51dd8ebe2b19e2928c4f6f21e83a298cfde4ef32
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-04-26 19:44:36 +0530

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,22 +1,38 @@
-import BestSeller from "../components/BestSeller"
-import Hero from "../components/Hero"
-import LatestCollection from "../components/LatestCollection"
-import NewsletterBox from "../components/NewsLetterBox"
-import OurPolicy from "../components/OurPolicy"
+import Hero from "../components/Hero";
+import LatestCollection from "../components/LatestCollection";
+import BestSeller from "../components/BestSeller";
+import OurPolicy from "../components/OurPolicy";
+import NewsletterBox from "../components/NewsLetterBox";
 
 const Home = () => {
   return (
-    <div>
-      <Hero />
-      <LatestCollection />
-      <BestSeller />
-      <OurPolicy />
-      <div className="bg-gray-100 py-4">
+    <div className="space-y-16">
+      {/* Hero Section */}
+      <section>
+        <Hero />
+      </section>
+
+      {/* Latest Products */}
+      <section className="">
+        <LatestCollection />
+      </section>
+
+      {/* Best Sellers */}
+      <section className="">
+        <BestSeller />
+      </section>
+
+      {/* Policies */}
+      <section className="">
+        <OurPolicy />
+      </section>
+
+      {/* Newsletter */}
+      <section className=" px-4 sm:px-6 lg:px-8 py-4">
         <NewsletterBox />
-      </div>
-
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
