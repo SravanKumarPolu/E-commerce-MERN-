f4b6c829a1105a602803fcf3880b0310373d7818
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-07-24 02:36:42 +0530

    Ensure Your Vite App Supports PWA Properly

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f001acab'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rq5m0jetj98"
+    "revision": "0.ep5pg7uvls8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
