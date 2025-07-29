539c130fae600e7a8e52d0b1724ee1c27b9b541a
SravanKumar Polu <99873642+SravanKumarPolu@users.noreply.github.com>
2025-07-30 01:00:11 +0530

    fix: require buffer-from in ESM mode for Render

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ import 'dotenv/config';
 
 
 // 👇 force preload to ensure Render doesn't skip it
-import 'buffer-from';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+require('buffer-from');
 
 import express from 'express';
 import cors from 'cors';
